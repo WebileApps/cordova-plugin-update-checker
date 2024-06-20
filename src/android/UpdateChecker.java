4cdb7d109f5744addc282db3d5baff27f6f1675b
SamuelNittala <nittalasamuel7@gmail.com>
2024-06-20 18:10:32 +0530

    Feat: Add logging

--- src/android/UpdateChecker.java
+++ src/android/UpdateChecker.java
@@ -1,7 +1,11 @@
 package com.example.updatechecker;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.app.AlertDialog;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -18,6 +22,8 @@ public class UpdateChecker extends CordovaPlugin {
   private Handler handler;
   private Runnable checkRunnable;
   private String updateCheckUrl;
+  private static final String TAG = "UpdateChecker";
+
 
   @Override
   protected void pluginInitialize() {
@@ -25,6 +31,7 @@ public class UpdateChecker extends CordovaPlugin {
     checkRunnable = new Runnable() {
       @Override
       public void run() {
+        Log.d(TAG, "Running update check");
         checkForUpdate();
         handler.postDelayed(this, CHECK_INTERVAL_MS);
       }
@@ -34,21 +41,26 @@ public class UpdateChecker extends CordovaPlugin {
   @Override
   public void onResume(boolean multitasking) {
     super.onResume(multitasking);
+    Log.d(TAG, "App resumed, starting update checker");
+    startUpdateChecker();
+  }
+
+  @Override
+  public void onReset() {
+    super.onReset();
+    Log.d(TAG, "App resumed, starting update checker");
     startUpdateChecker();
   }
 
   @Override
   public void onPause(boolean multitasking) {
     super.onPause(multitasking);
+    Log.d(TAG, "App paused, stopping update checker");
     stopUpdateChecker();
   }
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    if (action.equals("checkForUpdate")) {
-      checkForUpdate(callbackContext);
-      return true;
-    }
     if (action.equals("setUpdateCheckUrl")) {
       setUpdateCheckUrl(args.getString(0), callbackContext);
       return true;
@@ -58,48 +70,34 @@ public class UpdateChecker extends CordovaPlugin {
 
   private void setUpdateCheckUrl(String url, CallbackContext callbackContext) {
     updateCheckUrl = url;
+    Log.d(TAG, "Update check URL set to: " + url);
     callbackContext.success("URL set successfully");
-  }
-
-  private void checkForUpdate(CallbackContext callbackContext) {
-    cordova.getThreadPool().execute(() -> {
-      try {
-        String urlString = args.getString(0); // The URL should be passed as an argument
-        URL url = new URL(urlString);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("HEAD");
-        connection.connect();
-
-        long lastModified = connection.getLastModified();
-        callbackContext.success(Long.toString(lastModified));
-      } catch (IOException e) {
-        callbackContext.error("Failed to check for update: " + e.getMessage());
-      } catch (JSONException e) {
-        callbackContext.error("Invalid URL parameter: " + e.getMessage());
-      }
-    });
+    startUpdateChecker();
   }
 
   private void checkForUpdate() {
     if (updateCheckUrl == null || updateCheckUrl.isEmpty()) {
         // No URL set, nothing to do
+        Log.w(TAG, "No URL set for update checking");
         return;
     }
     cordova.getThreadPool().execute(() -> {
       try {
+        Log.d(TAG, "Checking for updates at URL: " + updateCheckUrl);
         URL url = new URL(updateCheckUrl);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("HEAD");
         connection.connect();
 
         long lastModified = connection.getLastModified();
+        Log.d(TAG, "Last modified time from server: " + lastModified);
         long storedTimestamp = Long
             .parseLong(cordova.getActivity().getPreferences(MODE_PRIVATE).getString("lastModified", "0"));
+        Log.d(TAG, "Stored last modified time: " + storedTimestamp);
 
         if (lastModified > storedTimestamp) {
           cordova.getActivity().runOnUiThread(() -> {
-            // Use WebView or any other method to display the update dialog
-            // For example, show an AlertDialog
+            Log.d(TAG, "Update available, prompting user to reload");
             new AlertDialog.Builder(cordova.getActivity())
                 .setTitle("Update Available")
                 .setMessage("A new version is available. Do you want to reload?")
@@ -112,8 +110,11 @@ public class UpdateChecker extends CordovaPlugin {
                 .show();
           });
         }
+        else {
+          Log.d(TAG, "No update available");
+        }
       } catch (IOException e) {
-        // Handle the error
+        Log.e(TAG, "Error checking for updates: ", e);
       }
     });
   }
@@ -126,3 +127,4 @@ public class UpdateChecker extends CordovaPlugin {
     handler.removeCallbacks(checkRunnable);
   }
 }
+
