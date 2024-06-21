b96e755034bb5fc8d7c0b4b958377ce9f409e134
SamuelNittala <nittalasamuel7@gmail.com>
2024-06-21 11:21:40 +0530

    use ConfigXmlParser to get the launch url

--- src/android/UpdateChecker.java
+++ src/android/UpdateChecker.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 
+import org.apache.cordova.ConfigXmlParser;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
@@ -70,28 +71,10 @@ public class UpdateChecker extends CordovaPlugin {
       callbackContext.success("Reload cancelled");
     }
   }
-
   private String getContentUrlFromConfig() {
-    String url = null;
-    try {
-      InputStream inputStream = cordova.getActivity().getAssets().open("www/config.xml");
-      XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-      XmlPullParser parser = factory.newPullParser();
-      parser.setInput(inputStream, "UTF-8");
-
-      int eventType = parser.getEventType();
-      while (eventType != XmlPullParser.END_DOCUMENT) {
-        if (eventType == XmlPullParser.START_TAG && "content".equals(parser.getName())) {
-          url = parser.getAttributeValue(null, "src");
-          break;
-        }
-        eventType = parser.next();
-      }
-      inputStream.close();
-    } catch (XmlPullParserException | IOException e) {
-      Log.e(TAG, "Error reading config.xml", e);
-    }
-    return url;
+    ConfigXmlParser parser = new ConfigXmlParser();
+    parser.parse(cordova.getActivity());
+    return parser.getLaunchUrl();
   }
 
   private void checkForUpdate() {
